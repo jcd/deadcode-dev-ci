5ea6c21699ec5ae67eebac6e88be137ca3fb2185
Jonas Drewsen <jonasd@unity3d.com>
2018-03-29 23:23:54 +0200

    fix

--- clone-dub-deps.pl
+++ clone-dub-deps.pl
@@ -1,6 +1,9 @@
+my @PACKS = ();
 $X=0; 
+
 while (1) {
-	$M=`find . -not -regex .*\.dub/dub.json -a -name dub.json | while read F; do cat \$F | perl -ne 'print if s/.*\\"(deadcode-.*)\\"\\s*?:.*/\\1/' ; done | sort | uniq | tee deps.txt | wc -l`;
+	$M=`find . -not -regex .*\.dub/dub.json -a -name dub.json | while read F; do cat \$F | " .
+	   "perl -ne 'print if s/.*\\"(deadcode-.*|unit-threaded|msgpack-d|derelict-.*|poodinis)\\"\\s*?:.*/\\1/' ; done | sort | uniq | tee deps.txt | wc -l`;
 	print "$X $M\n"; 
 	print `cat deps.txt` . "\n";
 	last if ($X == scalar($M)); 
@@ -9,6 +12,15 @@ while (1) {
 	open my $fh, '<', "deps.txt" or die "Cannot open deps.txt: $!";
 	
 	while ( my $line = <$fh> ) {
+		chomp $line;
+
+		if (grep( /^$line$/, @PACKS ))
+		{
+			continue;
+		}
+
+		push @PACKS, $line;
+
 		print "git clone https://github.com/jcd/${line}.git ${line}\n";
 		print `git clone https://github.com/jcd/${line}.git ${line}` . "\n";
 		print "dub add-local ${line}\n";
