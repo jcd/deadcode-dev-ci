53c585918afff2b5164ef9800ba1bfa61f1ed324
Jonas Drewsen <jonasd@unity3d.com>
2018-03-29 22:31:25 +0200

    Change impl

--- clone-dub-deps.pl
+++ clone-dub-deps.pl
@@ -2,9 +2,18 @@ $X=0;
 while (1) {
 	$M=`find . -not -regex .*\.dub/dub.json -a -name dub.json | while read F; do cat \$F | perl -ne 'print if s/.*\\"(deadcode-.*)\\"\\s*?:.*/\\1/' ; done | sort | uniq | tee deps.txt | wc -l`;
 	print "$X $M\n"; 
-	$DEPS=`cat deps.txt`;
-	print "$DEPS\n";
+	prin `cat deps.txt` . "\n";
 	last if ($X == scalar($M)); 
 	$X=scalar($M);
-	$HH=`cat deps.txt | while read I; do if [ ! -e \$I ] ; then git clone https://github.com/jcd/\$I.git \$I ; dub add-local \$I ; fi; done;`;
+
+	open my $fh, '<', "deps.txt" or die "Cannot open deps.txt: $!";
+	
+	while ( my $line = <$fh> ) {
+		print "git clone https://github.com/jcd/\${line}.git \${line}\n";
+		print `git clone https://github.com/jcd/\${line}.git \${line}` . "\n";
+		print "dub add-local \${line}\n";
+		print `dub add-local \${line}` . "\n";
+	}
+
+	close($fh);
 }
